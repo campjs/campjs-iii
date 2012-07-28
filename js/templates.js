9317b0df62b0e8502aaf1d9e9e0b15118385c1dd
Tim Oxley <secoif@gmail.com>
2012-07-28 20:03:43 +1000

    Initial site publish

---
+++ js/templates.js
@@ -0,0 +1,9 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['form'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  helpers = helpers || Handlebars.helpers;
+  var foundHelper, self=this;
+
+
+  return "<test></test>";});
+})();
