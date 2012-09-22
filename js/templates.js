cb7520dc2783250e632e928be2719edb107ff106
Tim Oxley <secoif@gmail.com>
2012-09-22 13:56:22 +1000

    Updated with temp site

--- js/templates.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['form'] = template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers;
-  var foundHelper, self=this;
-
-
-  return "<test></test>";});
-})();
